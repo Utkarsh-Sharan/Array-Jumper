23d792d5464f3e37e98959c12f2e487b09744ae7
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-09 10:54:31 +0530

    created player model

---
+++ Array-Jumper/source/Player/PlayerView.cpp
@@ -0,0 +1,62 @@
+#include "Player/PlayerView.h"
+
+namespace Player
+{
+	using namespace UI::UIElement;
+
+	PlayerView::PlayerView(/*PlayerController* playerController*/)
+	{
+		game_window = nullptr;
+		player_image = new ImageView();
+	}
+
+	PlayerView::~PlayerView()
+	{
+
+	}
+
+	void PlayerView::initialize()
+	{
+
+	}
+
+	void PlayerView::initializePlayerImage()
+	{
+
+	}
+
+	void PlayerView::drawPlayer()
+	{
+
+	}
+
+	void PlayerView::loadPlayer()
+	{
+
+	}
+
+	void PlayerView::calculatePlayerDimensions()
+	{
+
+	}
+
+	void PlayerView::update()
+	{
+
+	}
+
+	void PlayerView::updatePlayerPosition()
+	{
+
+	}
+
+	void PlayerView::render()
+	{
+
+	}
+
+	sf::Vector2f PlayerView::calculatePlayerPosition()
+	{
+		return sf::Vector2f();
+	}
+}
