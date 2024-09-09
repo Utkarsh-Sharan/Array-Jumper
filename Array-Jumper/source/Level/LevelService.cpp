3c7b91df8a3510984dd8831d09e0444fe6a10302
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-09-09 12:36:26 +0530

    created level service

---
+++ Array-Jumper/source/Level/LevelService.cpp
@@ -0,0 +1,37 @@
+#include "Level/LevelService.h"
+#include "Level/LevelController.h"
+
+namespace Level
+{
+	LevelService::LevelService()
+	{
+		level_controller = new LevelController();
+	}
+
+	LevelService::~LevelService() 
+	{ 
+		destroy(); 
+	}
+
+	void LevelService::intialize()
+	{
+		level_controller->initialize();
+	}
+
+	void LevelService::update()
+	{
+		level_controller->update();
+	}
+
+	void LevelService::render()
+	{
+		level_controller->render();
+	}
+
+	void LevelService::destroy()
+	{
+		level_controller = nullptr;
+
+		delete(level_controller);
+	}
+}
